ba2c692f337c04e265f4410ad7ee25c4546a9a0c
KovagoLevente <kovagolevente@turr.hu>
2024-12-04 10:29:52 +0100

    routes

--- Public/src/app/app.routes.ts
+++ Public/src/app/app.routes.ts
@@ -6,14 +6,18 @@ import { NotfoundComponent } from './components/notfound/notfound.component';
 import { RoominfoComponent } from './components/roominfo/roominfo.component';
 import { LostpassComponent } from './components/lostpass/lostpass.component';
 import { LogoutComponent } from './components/logout/logout.component';
+import { BookingsComponent } from './components/bookings/bookings.component';
+import { Component } from '@angular/core';
+import { ManageRoomsComponent } from './components/manage-rooms/manage-rooms.component';
+import { ManageBookingsComponent } from './components/manage-bookings/manage-bookings.component';
 
 export const routes: Routes = [
+
+  //logged out routes
   {
     path: 'login', component: LoginComponent
   },
-  {
-    path: 'logout', component: LogoutComponent
-  },
+  
   {
     path: 'registration', component: RegistrationComponent
   },
@@ -26,6 +30,30 @@ export const routes: Routes = [
   {
     path: 'rooms/:id', component: RoominfoComponent
   },
+  
+  //login
+  {
+    path: 'logout', component: LogoutComponent
+  },
+  //user
+  {
+    path:'bookings',component:BookingsComponent
+  },
+
+  //admin
+  {
+    path:'admin',
+    children:[{
+      path:'rooms', component:ManageRoomsComponent
+    },
+    {
+      path:'bookings', component:ManageBookingsComponent
+    }
+    ]
+    
+  },
+
+  //minden más
   {
     path: '', redirectTo: 'rooms', pathMatch: 'full'
   },
